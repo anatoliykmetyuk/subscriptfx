e2abee69bb5113296435b4026fea8799ff833c87
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-04-17 16:03:11 +0300

    comments

--- subscriptfx/src/main/scala/subscriptfx/package.scala
+++ subscriptfx/src/main/scala/subscriptfx/package.scala
@@ -13,6 +13,10 @@ import scalafx.{event => sfx}
 
 
 package object subscriptfx {
+  /**
+   * Keeps track of the handlers registered on certain ObjectProperty[EventHandler] from scripts.
+   * Once an event happens on that EventHandler, all the registered handlers are invoked.
+   */
   object Listeners {
     val listeners = mutable.Map[ObjectProperty[_ <: EventHandler[_]], mutable.Set[Event => Unit]]()
     
@@ -33,6 +37,12 @@ package object subscriptfx {
   }
 
   implicit script..
+
+    /**
+     * Converts an ObjectProperty[EventHandler] to a script. This script
+     * will wait for an event to happen on this EventHandler, then it will
+     * have success with the result value set to the event that have happened.
+     */
     op2script(handlerProp: ObjectProperty[_ <: EventHandler[_]]) =
       var event: sfx.Event = null
       @{
@@ -46,9 +56,18 @@ package object subscriptfx {
       }: {..}
       ^event
 
+    /**
+     * Experimental: converts an object with an onAction event handler to a script.
+     * `act` is equivalent to `act.onAction`.
+     */
     act2script(act: {def onAction: ObjectProperty[EventHandler[ActionEvent]]}) = act.onAction
 
+  /**
+   * Invokes `task` on the GUI thread. Waits for the code to be executed,
+   * then has success.
+   */
   script gui(task: => Unit) = SSPlatform.runAndWait(task)
+
 
   // Interesting thing below: the foo code doens't give the compile-time
   // error, but the bar code does (when handler is set to EventHandler[Event]).
