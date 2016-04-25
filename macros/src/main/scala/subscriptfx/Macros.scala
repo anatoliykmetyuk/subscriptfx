552989d5887575a5723402bc37ab01fa72707441
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-04-26 00:01:37 +0300

    Comments, cosmetic improvement

--- macros/src/main/scala/subscriptfx/Macros.scala
+++ macros/src/main/scala/subscriptfx/Macros.scala
@@ -10,28 +10,28 @@ import javafx.beans.property.ObjectProperty
 
 // SubScript
 import subscript.vm.Script
-
+import subscript.vm.model.template.TemplateNode
 
 object Macros {
 
-  implicit def jfxe2sfxe[T](target: ObjectProperty[T]): subscript.vm.model.template.TemplateNode.Child = macro jfxe2sfxeImpl[T]
-  def jfxe2sfxeImpl[T: c.WeakTypeTag](c: Context)(target: c.Expr[ObjectProperty[T]]): c.Expr[subscript.vm.model.template.TemplateNode.Child] = {
+  implicit def jfxe2sfxe[T](target: ObjectProperty[T]): TemplateNode.Child = macro jfxe2sfxeImpl[T]
+  def jfxe2sfxeImpl[T: c.WeakTypeTag](c: Context)(target: c.Expr[ObjectProperty[T]]): c.Expr[TemplateNode.Child] = {
     import c.universe._
 
     val tpe = weakTypeOf[T]  // typeTag[EventHandler[_ >: X]]; X is what we need to find
-    println(tpe)
 
+    // Actually, `tpe` is either EventHandler[_ >: X] or EventHandler[X]
     val bound: Type = tpe match {
-      case t: ExistentialType => t
+      case t: ExistentialType => t      // `tpe` is EventHandler[_ >: X]
         .quantified.head.typeSignature  // _ >: X
         .asInstanceOf[TypeBounds].lo    // X
 
-      case t => t.typeArgs.head
+      case t => t.typeArgs.head         // `tpe` is EventHandler[X]
     }
 
     val callTree = q"subscriptfx.op2script[$bound, scalafx.event.Event with scalafx.delegate.SFXDelegate[$bound]](${target.tree}.asInstanceOf[javafx.beans.property.ObjectProperty[javafx.event.EventHandler[_ >: $bound]]])"
     // subscript.DSL._maybeCall(${calleeName.tree}, $exprTree)
-    c.Expr[subscript.vm.model.template.TemplateNode.Child] {q"""subscript.DSL._maybeCall("macro_call", (here: subscript.vm.N_call[Any]) => $callTree)"""}
+    c.Expr[TemplateNode.Child] {q"""subscript.DSL._maybeCall("macro_call", (here: subscript.vm.N_call[Any]) => $callTree)"""}
   }
 
 }
